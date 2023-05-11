7b8a1f19f1f8a33ece8751ce2e748fcf21010aaa
cwha0212 <cwha02120212@gmail.com>
2023-05-11 13:50:59 +0900

    0511

---
+++ src/Compressed_to_Image.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import CompressedImage, Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class CompressedToImage:
+    def __init__(self):
+        self.bridge = CvBridge()
+        self.sub = rospy.Subscriber('/zed2i/zed_node/left_raw/image_raw_color/compressed', CompressedImage, self.callback)
+        self.pub = rospy.Publisher('/image', Image, queue_size=1)
+
+    def callback(self, data):
+        try:
+            img = self.bridge.compressed_imgmsg_to_cv2(data, "bgr8")
+            img_msg = self.bridge.cv2_to_imgmsg(img, "bgr8")
+            self.pub.publish(img_msg)
+        except CvBridgeError as e:
+            rospy.logerr(e)
+
+if __name__ == '__main__':
+    rospy.init_node('compressed_to_image')
+    compressed_to_image = CompressedToImage()
+    rospy.spin()
