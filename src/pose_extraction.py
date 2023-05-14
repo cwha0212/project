e0ff473558000cb3453767b34ce058e4b4d51b52
cwha0212 <cwha02120212@gmail.com>
2023-05-14 18:57:01 +0900

    1

--- src/pose_extraction.py
+++ src/pose_extraction.py
@@ -15,12 +15,12 @@ def callback(data):
     r00 = 2 * (q_w * q_w + q_x * q_x) - 1
     r01 = 2 * (q_x * q_y - q_w * q_z)
     r02 = 2 * (q_x * q_z + q_w * q_y)
-    r03 = y
+    r03 = x
 
     r10 = 2 * (q_x * q_y + q_w * q_z)
     r11 = 2 * (q_w * q_w + q_y * q_y) - 1
     r12 = 2 * (q_y * q_z - q_w * q_x)
-    r13 = -x
+    r13 = y
 
     r20 = 2 * (q_x * q_z - q_w * q_y)
     r21 = 2 * (q_y * q_z + q_w * q_x)
