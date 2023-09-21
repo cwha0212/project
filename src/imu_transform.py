027863a6e7246761474fee581bdc95bd9de7fa93
cwha0212 <cwha02120212@gmail.com>
2023-09-21 17:04:12 +0900

    11

---
+++ src/imu_transform.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Imu
+
+def callback(data):
+    new_data = Imu()
+    new_data = data
+    new_data.angular_velocity.y = 0
+    new_data.linear_acceleration.y = -9.8
+    pub.publish(new_data)
+
+rospy.init_node('new_imu')
+pub = rospy.Publisher('publish_imu', Imu, queue_size=100)
+sub = rospy.Subscriber('/camera/imu', Imu, callback)
+rospy.spin()
